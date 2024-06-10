d91be57db2927daa5e1372d94285e7dd4d470fa6
Alyssa Choi <alyssa.seoyoon@gmail.com>
2024-06-10 14:43:12 -0400

    added todo and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import ProfilePic from '../assets/profile-pic.jpg';
+import './About.css';
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div className="split left">
+          <div className="centered">
+            <img 
+            className="profile_image"
+            src={ProfilePic}
+            alt="Profile Pic"
+            ></img>
+          </div>
+          </div>
+          <div className="split right">
+          <div className="centered">
+          <div className="name_title">Alyssa Choi</div>
+          <div className="brief_description">
+          I am interested in the ML/AI field. A fun fact about me is that I am a Pilates instructor.
+          </div>
+          </div>
+          </div>
+        </div>
       </div>
     )
   }
